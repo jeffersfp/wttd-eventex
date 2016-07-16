5c7eea2fa6b4f82f2e2d82ee756e38dc61748e44
Jefferson Pires <jeffersfp@gmail.com>
2016-07-16 17:20:39 -0300

    Generate new migrations to reflect the changes at Subscription data

--- eventex/subscriptions/migrations/0001_initial.py
+++ eventex/subscriptions/migrations/0001_initial.py
@@ -22,6 +22,7 @@ class Migration(migrations.Migration):
                 ('email', models.EmailField(max_length=254)),
                 ('phone', models.CharField(max_length=20)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
+                
             ],
         ),
     ]
